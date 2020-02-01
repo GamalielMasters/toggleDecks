049e0f36a7e8ffbaab2ef9ddb9a4844d49f06a39
Gamaliel Masters <g.masters@nwc3.com>
2020-02-01 15:49:58 -0800

    Refactored deck to struct and factored out deck creation.

--- card_deck.go
+++ card_deck.go
@@ -8,44 +8,61 @@ import (
 /*
 
  */
+
 var SUITES = [4]string{"S", "D", "C", "H"}
 var RANKS = [13]string{"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"}
 
+func LegalCodes() (cards [52]string) {
+	idx := 0
+
+	for _, suite := range SUITES {
+		for _, rank := range RANKS {
+			cards[idx] = rank + suite
+			idx++
+		}
+	}
+
+	return
+}
+
 type Card string
-type Deck []Card
 
 func (c Card) String() string {
 	return string(c)
 }
 
-func (d Deck) String() string {
+type Deck struct {
+	Cards    []Card
+	Shuffled bool
+}
+
+func (d Deck) Len() int {
+	return len(d.Cards)
+}
+
+func (d *Deck) String() string {
 	s := ""
-	for _, c := range d {
+	for _, c := range d.Cards {
 		s += c.String() + " "
 	}
 
 	return s[:len(s)-1]
 }
 
-func (d Deck) Swap(i, j int) {
-	d[i], d[j] = d[j], d[i]
+func (d *Deck) Swap(i, j int) {
+	d.Cards[i], d.Cards[j] = d.Cards[j], d.Cards[i]
 }
 
-func (d Deck) Shuffle() Deck {
+func (d *Deck) Shuffle() {
 	r := rand.New(rand.NewSource(time.Now().Unix()))
-	//return Shuffle{r.Perm( len(d) ), &d}
-	r.Shuffle(len(d), d.Swap)
-	return d
+	r.Shuffle(d.Len(), d.Swap)
+	d.Shuffled = true
 }
 
 func CreateDeck() (cards Deck) {
-	cards = Deck(make([]Card, 52))
-	idx := 0
-	for _, suite := range SUITES {
-		for _, rank := range RANKS {
-			cards[idx] = Card(rank + suite)
-			idx++
-		}
+	cards = Deck{make([]Card, 52), false}
+	for idx, code := range LegalCodes() {
+		cards.Cards[idx] = Card(code)
 	}
 
 	return
--- tests/card_deck_test.go
+++ tests/card_deck_test.go
@@ -9,10 +9,30 @@ import (
 
 const STANDARD_DECK = "AS 2S 3S 4S 5S 6S 7S 8S 9S 10S JS QS KS AD 2D 3D 4D 5D 6D 7D 8D 9D 10D JD QD KD AC 2C 3C 4C 5C 6C 7C 8C 9C 10C JC QC KC AH 2H 3H 4H 5H 6H 7H 8H 9H 10H JH QH KH"
 
+// Helper Functions
+
+func DeckToSSortedString(d toggleDecks.Deck) string {
+	return SortDeckString(d.String())
+}
+
+func SortDeckString(expected string) string {
+	sortedStrings := strings.Split(expected, " ")
+	sort.Strings(sortedStrings)
+	return strings.Join(sortedStrings, " ")
+}
+
+func DeckContainsCards(d toggleDecks.Deck, expected string) (bool, string, string) {
+	the_deck := DeckToSSortedString(d)
+	expected_deck := SortDeckString(expected)
+	return the_deck == expected_deck, expected_deck, the_deck
+}
+
+// Tests
+
 func TestDefaultDeckIs52Cards(t *testing.T) {
 	deck := toggleDecks.CreateDeck()
-	if len(deck) != 52 {
-		t.Errorf("Wanted %v cards in deck, but got %v instead.", 52, len(deck))
+	if deck.Len() != 52 {
+		t.Errorf("Wanted %v cards in deck, but got %v instead.", 52, deck.Len())
 	}
 }
 
@@ -33,24 +53,21 @@ func TestDefaultDeckForCorrectOrder(t *testing.T) {
 
 func TestShuffledDeckIsNotInInitialOrder(t *testing.T) {
 	deck := toggleDecks.CreateDeck()
-	shuffle := deck.Shuffle()
-	if shuffle.String() == STANDARD_DECK {
+	deck.Shuffle()
+	if deck.String() == STANDARD_DECK {
 		t.Errorf("Deck is not shuffled.")
 	}
 }
 
-func DeckToSSortedString(d toggleDecks.Deck) string {
-	return SortDeckString(d.String())
-}
+func TestDeckKnowsIfItHasBeenShuffled(t *testing.T) {
+	unshuffled_deck := toggleDecks.CreateDeck()
+	if unshuffled_deck.Shuffled {
+		t.Error("Unshuffled Deck thinks it's shuffdled.")
+	}
 
-func SortDeckString(expected string) string {
-	sortedStrings := strings.Split(expected, " ")
-	sort.Strings(sortedStrings)
-	return strings.Join(sortedStrings, " ")
-}
-
-func DeckContainsCards(d toggleDecks.Deck, expected string) (bool, string, string) {
-	the_deck := DeckToSSortedString(d)
-	expected_deck := SortDeckString(expected)
-	return the_deck == expected_deck, expected_deck, the_deck
+	shuffled_deck := toggleDecks.CreateDeck()
+	shuffled_deck.Shuffle()
+	if !shuffled_deck.Shuffled {
+		t.Error("Shuffled Deck thinks it's unshuffled.")
+	}
 }

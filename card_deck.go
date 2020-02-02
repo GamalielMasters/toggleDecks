fda904d95e77385b875d44754b12d4884a63c8e1
Gamaliel Masters <g.masters@nwc3.com>
2020-02-01 18:21:06 -0800

    Added ability for cards know their suite and rank.

--- card_deck.go
+++ card_deck.go
@@ -20,10 +20,27 @@ import (
 // Spades, Diamonds, Clubs, Hearts.  This can be used to generate a standard "french" deck of cards.
 const STANDARD_DECK = "AS 2S 3S 4S 5S 6S 7S 8S 9S 10S JS QS KS AD 2D 3D 4D 5D 6D 7D 8D 9D 10D JD QD KD AC 2C 3C 4C 5C 6C 7C 8C 9C 10C JC QC KC AH 2H 3H 4H 5H 6H 7H 8H 9H 10H JH QH KH"
 
+var SuiteMap = map[string]string{"S": "SPADES", "D": "DIAMONDS", "C": "CLUBS", "H": "HEARTS"}
+var RankMap = map[string]string{"A": "ACE", "1": "1", "2": "2", "3": "3", "4": "4", "5": "5", "6": "6", "7": "7", "8": "8", "9": "9", "10": "10", "J": "JACK", "Q": "QUEEN", "K": "KING"}
+
 type Card string
 
 func (c Card) String() string {
 	return string(c)
+}
+
+func (c Card) Code() string {
+	return c.String()
+}
+
+func (c Card) Rank() string {
+	code := c.Code()
+	return RankMap[code[:len(code)-1]]
+}
+
+func (c Card) Suite() string {
+	code := c.Code()
+	return SuiteMap[code[len(code)-1:]]
 }
 
 type Deck struct {
--- tests/card_deck_test.go
+++ tests/card_deck_test.go
@@ -25,7 +25,28 @@ func DeckContainsCards(d toggleDecks.Deck, expected string) (bool, string, strin
 	return the_deck == expected_deck, expected_deck, the_deck
 }
 
-// Tests
+// Card Tests
+
+func TestCardKnowsItsRank(t *testing.T) {
+	card := toggleDecks.Card("AC")
+	card2 := toggleDecks.Card("10D")
+	if card.Rank() != "ACE" {
+		t.Errorf("Card reports wrong rank.  Expected 'ACE', got '%v'", card.Rank())
+	}
+
+	if card2.Rank() != "10" {
+		t.Errorf("Card reports wrong rank.  Expected '10', got '%v'", card2.Rank())
+	}
+}
+
+func TestCardKnowsItsSuite(t *testing.T) {
+	card := toggleDecks.Card("AC")
+	if card.Suite() != "CLUBS" {
+		t.Errorf("Card reports wrong suite. Expected 'CLUBS', got '%v'", card.Suite())
+	}
+}
+
+// Deck Tests
 
 func TestDefaultDeckIs52Cards(t *testing.T) {
 	deck := toggleDecks.CreateFullDeck()
